0f143775e0b63c196648730f5d100dd37f72aca4
Debora <deboraharesende94@gmail.com>
2017-04-10 16:50:28 -0300

    removeeeeu teste

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,5 @@
 import javax.swing.JOptionPane;
-
+// teste
 public class Principal {
 
 	public static void main (String[] args){
@@ -8,7 +8,7 @@ public class Principal {
 		do {
 			opcao = JOptionPane.showInputDialog("1 - Cadastrar Avião\n2 - Editar Avião\n3 - Excluir Avião\n"
 					+ "4 - Cadastrar Passageiro\n5 - Editar Passageiro\n6 - Excluir Passageiro\n"
-                    + "7 - Cadastrar Voo\n8 - Editar Voo\n9 - Excluir Voo\n10 - ");
+                    + "7 - Cadastrar Voo\n8 - Editar Voo\n9 - Excluir Voo\n10");
             if (opcao != null) {
 				if (opcao.equals("1")) {
 					// addAluno (lista);
