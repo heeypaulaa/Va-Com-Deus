bd125b56bdd7b846ecc6948c5593df979254db92
paula <heeypaulaa@gmail.com>
2017-04-10 16:47:24 -0300

    test

--- src/Principal.java
+++ src/Principal.java
@@ -8,8 +8,8 @@ public class Principal {
 		do {
 			opcao = JOptionPane.showInputDialog("1 - Cadastrar Avião\n2 - Editar Avião\n3 - Excluir Avião\n"
 					+ "4 - Cadastrar Passageiro\n5 - Editar Passageiro\n6 - Excluir Passageiro\n"
-					+ "7 - Cadastrar Voo\n8 - Editar Voo\n9 - Excluir Voo\n");
-			if (opcao != null) {
+                    + "7 - Cadastrar Voo\n8 - Editar Voo\n9 - Excluir Voo\n10 - ");
+            if (opcao != null) {
 				if (opcao.equals("1")) {
 					// addAluno (lista);
 				} else if (opcao.equals("2")) {
